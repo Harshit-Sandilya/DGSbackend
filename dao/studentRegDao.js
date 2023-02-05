70fdcb8d5163a6f18b50f2cb9ddae8c4a9008ed0
RUPESH <89046490+rupesh031@users.noreply.github.com>
2023-02-05 11:02:06 +0530

    build

--- dao/studentRegDao.js
+++ dao/studentRegDao.js
@@ -1,39 +1,40 @@
+const e = require("cors");
 const sendEmail = require("../mailer");
 const Student = require("../models/StudentReg");
 
 class studentRegDAO {
-	static async addStudent(res, data) {
-		try {
-			const newStudent = new Student(data);
-			newStudent
-				.save()
-				.then(() => {
-					console.log("Student Added Success");
-					sendEmail({
-						subject: "Student Registration",
-						text: `${newStudent}`,
-						to: "dundlodgirlsschool@gmail.com",
-						from: process.env.EMAIL,
-						replyTo: `${data.email}`,
-					});
-					sendEmail({
-						subject: "Student Registration",
-						text: `${newStudent}`,
-						to: `${data.email}`,
-						from: process.env.EMAIL,
-						replyTo: "dundlodgirlsschool@gmail.com",
-					});
-					res.status(200).json({ message: "success" });
-				})
-				.catch((err) => {
-					res.status(500).json({ error: err });
-					return;
-				});
-			console.log(newStudent);
-		} catch (err) {
-			res.status(500).json({ error: err });
-		}
-	}
+  static async addStudent(res, data) {
+    try {
+      const newStudent = new Student(data);
+      newStudent
+        .save()
+        .then(() => {
+          console.log("Student Added Success");
+          sendEmail({
+            subject: "Student Registration",
+            text: `${newStudent}`,
+            to: "dundlodgirlsschool@gmail.com",
+            from: process.env.EMAIL,
+            replyTo: `${data.email}`,
+          });
+          sendEmail({
+            subject: "Student Registration",
+            text: `${newStudent}`,
+            to: `${data.email}`,
+            from: process.env.EMAIL,
+            replyTo: "dundlodgirlsschool@gmail.com",
+          });
+          res.status(200).json({ message: "success" });
+        })
+        .catch((err) => {
+          res.status(500).json({ error: "Unable to Register" });
+          return;
+        });
+      console.log(newStudent);
+    } catch (err) {
+      res.status(500).json({ error: err });
+    }
+  }
 }
 
 module.exports = studentRegDAO;
