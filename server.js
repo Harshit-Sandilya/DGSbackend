1e5b7de764a24b30f8b7ef5dd156c42102d51522
RUPESH <89046490+rupesh031@users.noreply.github.com>
2023-02-23 12:48:15 +0530

    cors

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const studentReg = require("./routes/studentReg.route.js");
 const teacherApp = require("./routes/teacherApp.route.js");
 const contact = require("./routes/contact.route.js");
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/studentReg", studentReg);
 app.use("/teacherApp", teacherApp);
 app.use("/contact", contact);
 app.use("/", (req, res) => res.status(200).json({ message: "running" }));
-app.use("*", (req, res) => res.status(404).json({ error: "Not found" }));
+// app.use("*", (req, res) => res.status(404).json({ error: "Not found" }));
 
 module.exports = app;
